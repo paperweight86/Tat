5609349350cece644aea894af3b994d4865137c4
paperweight86 <paperweight86dev@gmail.com>
2016-07-24 21:37:55 +0100

    Adding some float3 operators

--- inc/float3.h
+++ inc/float3.h
@@ -11,9 +11,15 @@ namespace uti
 	TAT_DEF float3 operator-(float3 left, float3 right);
 	TAT_DEF float3 operator+(float3 left, float3 right);
 	TAT_DEF float3 operator*(float3 left, float3 right);
+	TAT_DEF float3 operator*(float3 left, float right);
+	TAT_DEF float3 operator*(float left, float3 right);
 	TAT_DEF float3 operator/(float3 left, float3 right);
+	TAT_DEF float3 operator/(float3 left, float right);
 
 	TAT_DEF float3 dot  (float3 left, float3 right);
 	TAT_DEF float3 cross(float3 left, float3 right);
+
+	TAT_DEF float  len(float3 vec);
+	TAT_DEF float3 norm(float3 vec);
 }
 
--- src/float3.cpp
+++ src/float3.cpp
@@ -32,12 +32,39 @@ float3 operator*(float3 left, float3 right)
 	return result;
 }
 
+float3 operator*(float3 left, float right)
+{
+	float3 result;
+	result.x = left.x * right;
+	result.y = left.y * right;
+	result.z = left.z * right;
+	return result;
+}
+
+float3 operator*(float left, float3 right)
+{
+	float3 result;
+	result.x = left * right.x;
+	result.y = left * right.y;
+	result.z = left * right.z;
+	return result;
+}
+
 float3 operator/(float3 left, float3 right)
 {
 	float3 result;
 	result.x = left.x / right.x;
 	result.y = left.y / right.y;
 	result.z = left.z / right.z;
+	return result;
+}
+
+float3 operator/(float3 left, float right)
+{
+	float3 result;
+	result.x = left.x / right;
+	result.y = left.y / right;
+	result.z = left.z / right;
 	return result;
 }
 
@@ -57,6 +84,16 @@ float3 cross(float3 left, float3 right)
 	result.y = left.x*right.z - left.z*right.x;
 	result.z = left.x*right.y - left.y*right.x;
 	return result;
+}
+
+float  len(float3 vec)
+{
+	return sqrt(vec.x*vec.x + vec.y*vec.y + vec.z*vec.z);
+}
+
+float3 norm(float3 vec)
+{
+	return vec / len(vec);
 }
 
 }
