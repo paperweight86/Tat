f8c98d1eb1942d5b2ff652bad5a1482fbf446176
Dan <paperweight86dev@gmail.com>
2019-02-07 21:48:01 +0000

    Partial linux window impl

--- src/window_linux.cpp
+++ src/window_linux.cpp
@@ -7,32 +7,87 @@
 
 using namespace uti;
 
-namespace
+struct linux_window
 {
-	ptr window_process_message(window* win, uint32 message, uint64 wParam, uint64 lParam)
-	{
-		//win->keyboard_callback(wParam, keyDown, false);
-		//win->mouse_callback(xPos, yPos, lButton, mButton, rButton, 0, win);
-		//win->drop_file_callback(files, num_files, MAX_PATH, f2pt);
-		return u64_max;
-	}
-}
+	Display* display;
+	Window   window;
+};
 
 bool uti::window_initialise(window* win, int16 width, int16 height, bool show, tstr windowTitle)
 {
-	
+	auto lwin = new linux_window;
+	memset(lwin, 0, sizeof(linux_window));
+	lwin->display = XOpenDisplay(nullptr);
+    if (lwin->display == nullptr)
+    {
+    	delete lwin;
+        return false;
+    }
 
-	return false;
+    int def_screen = DefaultScreen(lwin->display);
+
+    lwin->window = XCreateSimpleWindow(
+    	lwin->display,
+    	RootWindow(lwin->display, def_screen),
+    	10, 10, 200, 200, 1,
+    	BlackPixel(lwin->display, def_screen),
+    	WhitePixel(lwin->display, def_screen)
+    	);
+
+    XSelectInput(lwin->display, lwin->window,
+    	ExposureMask | KeyPressMask | KeyReleaseMask
+    	);
+
+    XMapWindow(lwin->display, lwin->window);
+
+    win->hwnd = (uti::ptr)lwin;
+
+	return true;
 }
 
 bool uti::window_update(window* win)
 {
+	auto lwin = (linux_window*)win->hwnd;
+
+	XEvent event;
+	while(XCheckMaskEvent(
+			lwin->display, 
+			ExposureMask | KeyPressMask | KeyReleaseMask, 
+			&event
+		 ))
+	{
+		switch(event.type)
+		{
+			case Expose:
+
+				break;
+
+			case KeyPress:
+				if(win->keyboard_callback)
+				{
+					win->keyboard_callback(event.xkey.keycode, true, false);
+				}
+				break;
+
+			case KeyRelease:
+				if(win->keyboard_callback)
+				{
+					win->keyboard_callback(event.xkey.keycode, false, false);
+				}
+				break;
+		}
+	}
+
+	//win->mouse_callback(xPos, yPos, lButton, mButton, rButton, 0, win);
+	//win->drop_file_callback(files, num_files, MAX_PATH, f2pt);
+
 	return false;
 }
 
 void uti::window_show(window* win)
 {
-	
+	auto lwin = (linux_window*)win->hwnd;
+	XMapWindow(lwin->display, lwin->window);
 }
 
 void uti::window_close(window* win)
